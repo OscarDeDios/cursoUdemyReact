879d7079e9a7e038b9eca23c2a3ce2e1984b2251
Oscar de Dios <oscar.dedios.rodriguez@gmail.com>
2017-09-06 18:34:31 +0200

    com

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,10 +4,18 @@ class SearchBar extends Component {
 	constructor(props) {
 		super(props);
 
-		this.state = {term: ''};
+		this.state = {term: 'Prueba'};
 	}
+	
 	render() {
-		return <input onChange={this.onInputChange} />;
+		return (
+			<div>
+				<input 
+					value={this.state.term}
+					onChange={event => this.setState({ term: event.target.value})} />
+				Value of the input: {this.state.term}
+			</div>
+		)
 	}
 
 	onInputChange(event) {
--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyBjebhdbNMoW2nI-RCk8-34e_mFcBZOEkQ';
+
+YTSearch({key: API_KEY, term: 'guitars'}, function(data) {
+	console.log(data);
+});
 
 const App = () => {
 	return (
