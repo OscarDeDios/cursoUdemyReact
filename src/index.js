90210cb3d4343093070a19ee69c081e2fff86f67
Oscar de Dios <oscar.dedios.rodriguez@gmail.com>
2017-09-07 19:07:43 +0200

    Video 23

--- src/index.js
+++ src/index.js
@@ -1,21 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDom from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
+import VideoList from './components/video_list';
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyBjebhdbNMoW2nI-RCk8-34e_mFcBZOEkQ';
 
-YTSearch({key: API_KEY, term: 'guitars'}, function(data) {
-	console.log(data);
-});
 
-const App = () => {
-	return (
-		<div>
-			<SearchBar></SearchBar>
-		</div>
-	);
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {videos: []};
+
+		YTSearch({key: API_KEY, term: 'guitars'}, (videos) => {
+			this.setState( {videos});
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				<SearchBar />
+				<VideoList videos={this.state.videos} />
+			</div>
+		);
+	}
 }
+
 	
 ReactDom.render(<App />, document.querySelector('.container'));
