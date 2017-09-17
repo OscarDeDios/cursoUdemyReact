d346a368f266a935099b28eafd4cd0645626b8e8
Oscar de Dios <oscar.dedios.rodriguez@gmail.com>
2017-09-17 12:26:51 +0200

    Redux - BookList (sección 4)

--- Redux-BookList/src/reducers/reducer_books.js
+++
@@ -1,8 +0,0 @@
-export default function() {
-    return [
-        {title: 'Javascript: The good Parts', pages: 101},
-        {title: 'Harry Potter', pages: 142},
-        {title: 'The Dark Tower', pages: 41},
-        {title: 'Eloquent Ruby', pages: 52},
-    ]
-}
