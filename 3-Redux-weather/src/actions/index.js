353db2cbfc98061b570ab3605474b38cb5d7db92
Oscar de Dios <oscar.dedios.rodriguez@gmail.com>
2017-09-22 12:18:15 +0200

    video 62

--- 3-Redux-weather/src/actions/index.js
+++ 3-Redux-weather/src/actions/index.js
@@ -1,1 +1,16 @@
-const API_KEY = "789be5b45b7147e702795964edfcac6f";
+import axios from 'axios';
+
+const API_KEY = '789be5b45b7147e702795964edfcac6f';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fecthWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    }
+}
--- 3-Redux-weather/src/containers/search_bar.js
+++ 3-Redux-weather/src/containers/search_bar.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators} from 'redux';
+import { fecthWeather} from '../actions/index';
 
-export default class SearchBar extends Component {
+
+class SearchBar extends Component {
     constructor(props) {
         super(props);
 
         this.state = {term: ''};
         this.onInputChange = this.onInputChange.bind(this);
+        this.onFormSubmit = this.onFormSubmit.bind(this);
     }
 
     onInputChange(event) {
@@ -16,6 +21,8 @@ export default class SearchBar extends Component {
         event.preventDefault();
 
         // Fecth weather data
+        this.props.fecthWeather(this.state.term);
+        this.setState({ term: '' });
     }
 
     render() {
@@ -34,3 +41,9 @@ export default class SearchBar extends Component {
         );
     }
 }
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators( {fecthWeather}, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)( SearchBar);
