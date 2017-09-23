0f8d1659bf85e36466dffb11f900d62fbf8811d8
Oscar de Dios <oscar.dedios.rodriguez@gmail.com>
2017-09-23 18:06:57 +0200

    Weather End

--- 3-Redux-weather/src/reducers/reducer_weather.js
+++ 3-Redux-weather/src/reducers/reducer_weather.js
@@ -3,6 +3,7 @@ import { FETCH_WEATHER } from '../actions/index';
 export default function (state = [], action) {
     switch (action.type) {
         case FETCH_WEATHER:
+            // nnca se modifica directamente el state, siempre hay que devolver un nuevo objeto construido con el state previo
             //return state.concat([action.payload.data]);
             return [ action.payload.data, ...state];
     }
